90fa0443bda1bd204120c13a06714c525dc8e7ef
유림 <yulimh001@gmail.com>
2026-04-17 16:55:47 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/test/java/com/naleul/naleul/NaleulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naleul.naleul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaleulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
