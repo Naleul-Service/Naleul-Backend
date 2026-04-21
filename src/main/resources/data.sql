29de85a3cff3f5db559250d4777a2cab7cff44a8
유림 <yulimh001@gmail.com>
2026-04-21 15:31:49 +0900

    feat: data.sql로 요일 테이블 정의

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO day_of_week (day_name, day_order) VALUES
+('MONDAY',    1),
+('TUESDAY',   2),
+('WEDNESDAY', 3),
+('THURSDAY',  4),
+('FRIDAY',    5),
+('SATURDAY',  6),
+('SUNDAY',    7);
