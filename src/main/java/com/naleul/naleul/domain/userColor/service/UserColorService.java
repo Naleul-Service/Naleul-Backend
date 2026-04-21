6d0153feaa13643ef62775e8df57225e26e5fd81
유림 <yulimh001@gmail.com>
2026-04-21 14:25:47 +0900

    fix: data.sql 제거 및 DefaultColor 엔티티 사용

--- src/main/java/com/naleul/naleul/domain/userColor/service/UserColorService.java
+++ src/main/java/com/naleul/naleul/domain/userColor/service/UserColorService.java
@@ -5,6 +5,7 @@ package com.naleul.naleul.domain.userColor.service;
 import com.naleul.naleul.domain.user.entity.User;
 import com.naleul.naleul.domain.user.repository.UserRepository;
 import com.naleul.naleul.domain.userColor.dto.response.UserColorResponse;
+import com.naleul.naleul.domain.userColor.entity.DefaultColors;
 import com.naleul.naleul.domain.userColor.entity.UserColor;
 import com.naleul.naleul.domain.userColor.repository.UserColorRepository;
 import com.naleul.naleul.global.common.response.ErrorCode;
@@ -28,18 +29,7 @@ public class UserColorService {
                 .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 
-    public class DefaultColors {
-        public static final List<String> CODES = List.of(
-                "#FF0000", "#FF5733", "#FF8C00",
-                "#FFD700", "#ADFF2F", "#00FF00",
-                "#00FA9A", "#00FFFF", "#1E90FF",
-                "#0000FF", "#8A2BE2", "#FF00FF",
-                "#FF69B4", "#A52A2A", "#808080",
-                "#000000", "#FFFFFF", "#F5F5DC",
-                "#40E0D0", "#FF7F50", "#6495ED"
-        );
-    }
-
+    @Transactional
     public void createDefaultColors(User user) {
         List<UserColor> defaultColors = DefaultColors.CODES.stream()
                 .map(code -> UserColor.builder()
