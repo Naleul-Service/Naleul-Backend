6542ec48d5b5ba4bd39a7122011d5681d9d9dc22
유림 <yulimh001@gmail.com>
2026-04-22 19:56:06 +0900

    fix: data.sql 구문 삭제

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT IGNORE INTO day_of_week (day_name, day_order) VALUES
-('MONDAY',    1),
-('TUESDAY',   2),
-('WEDNESDAY', 3),
-('THURSDAY',  4),
-('FRIDAY',    5),
-('SATURDAY',  6),
-('SUNDAY',    7);
